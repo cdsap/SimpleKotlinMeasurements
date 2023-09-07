d7877e03cac8b2c0297c20aaa359eb8596015a2b
Inaki Villar <inaki.seri@gmail.com>
2023-09-07 15:20:16 -0700

    init

---
+++ build-logic/convention/src/main/kotlin/com/logic/Plugin1.kt
@@ -0,0 +1,22 @@
+package com.logic
+
+import org.gradle.api.Plugin
+import org.gradle.api.JavaVersion
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.withType
+
+class Plugin1 : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("org.jetbrains.kotlin.jvm")
+            }
+        }
+
+        target.tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            kotlinOptions {
+                jvmTarget = JavaVersion.VERSION_17.toString()
+            }
+        }
+    }
+}
