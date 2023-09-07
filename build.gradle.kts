40c5c40a9d35fe8e71386d9c6ab692247e611913
Inaki Villar <inaki.seri@gmail.com>
2023-09-07 16:25:31 -0700

    adding gradle properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+  id("io.github.cdsap.kotlinprocess") version "0.1.5"
     id("com.autonomousapps.dependency-analysis") version "1.21.0"
     id("org.jetbrains.kotlin.jvm") version("1.9.10") apply false
 }
